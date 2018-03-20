2b31c88173b48ca0056729a1ab9daa51052a59a6
Robert Cooper <bcoop713@gmail.com>
2018-03-20 00:17:23 -0400

    incremented version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Moonsugar.MixProject do
   def project do
     [
       app: :moonsugar,
-      version: "0.1.1",
+      version: "0.1.2",
       elixir: "~> 1.5",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
